9287453127c23aea5198bd8cd10bc40cc1df987c
Nodiru Gaji <c0d3r.nodiru.gaji@gmail.com>
2021-12-26 20:46:02 +0300

    Simple flip card

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # furontoendo
 sh1chan's frontend (UI)
+```console
+# simple flip card by onclick
+elm reactor
+# http://localhost:8000/src/Main.elm
+```
--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,52 @@
 module Main exposing (main)
 
-import Html exposing (text)
+import Browser
+import Html exposing (Html, p, div, img, text)
+import Html.Attributes exposing (src, width, height)
+import Html.Events exposing (onClick)
+
+
+type CardData
+  = Face
+  | Back
+
+type alias Model
+  = Int
+
+
+init : Model
+init =
+  0
+
+
+update : CardData -> Model -> Model
+update msg model =
+  case msg of
+    Face ->
+      model - 1
+    Back ->
+      model + 1
+
+
+card_img card_id =
+  div [ onClick Back ]
+  [ img [ src "4rut41ru.jpg", width 300, height 300 ] []
+  ]
+
+
+card_information card_id =
+  div [ onClick Face ]
+  [ p [] [ text "Card Information" ]
+  ]
+
+
+view : Model -> Html CardData
+view model =
+  if model == 0 then
+    card_img 1
+  else
+    card_information 1
+
 
 main =
-  text "Hello, World!"
+  Browser.sandbox { init = init, update = update, view = view }
